2fb0bb0bb66c9a84a52c26e0bbbb118a3ae9de7d
HemantWD <hemantajaysharma@gmail.com>
2023-08-02 23:57:08 +0530

    login and register connected to backend

---
+++ Frontend/reserve/src/Pages/Payment.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Header } from "../components/Layout/Header";
+import Footer from "../components/Layout/Footer";
+import { BusBooking } from "../components/AvailableBus/BusBooking";
+
+export const Payment = () => {
+  return (
+    <>
+      <Header />
+      <BusBooking />
+      <Footer />
+    </>
+  );
+};
